f136a7acd272ab01eac3baed508e3aac44633b35
Adil Lari <mazacon@pm.me>
2024-01-01 21:35:28 -0600

    Give puma time to shutdown

--- deploy/maz.sh
+++ deploy/maz.sh
@@ -4,4 +4,5 @@ pkill -f maz.dev
 cd ../maz.dev
 git stash
 git pull -f
+sleep 5
 ruby main.rb > /dev/null 2>&1 &
--- deploy/whman.sh
+++ deploy/whman.sh
@@ -5,4 +5,5 @@ pkill -f webhook-manager
 git stash
 git pull -f
 bundle install
+sleep 5
 ruby main.rb
