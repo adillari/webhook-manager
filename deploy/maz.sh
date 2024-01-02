72a22ed72939821119d8f3dde48b825b231ba5e5
Adil Lari <mazacon@pm.me>
2024-01-01 21:31:19 -0600

    Update deploy scripts

--- deploy/maz.sh
+++ deploy/maz.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-
-echo "Deploying maz.dev"
+#!/bin/sh
 
 pkill -f maz.dev
-if [ $? -eq 0 ]; then
-  echo "Killed maz.dev"
-else
-  echo "maz.dev not running"
-fi
 cd ../maz.dev
-echo "Should be in maz.dev"
-pwd
 git stash
 git pull -f
 ruby main.rb > /dev/null 2>&1 &
--- deploy/whman.sh
+++ deploy/whman.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Deploying self"
-sleep 8
+sleep 5
 pkill -f webhook-manager
-if [ $? -eq 0 ]; then
-  echo "Killed whman"
-else
-  echo "failed to kill whman"
-fi
-echo "Should be in same dir"
-pwd
 git stash
 git pull -f
 bundle install
