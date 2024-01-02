7a95f10e52b83dbe95442bc1c4fe99b48ecae5a1
Adil Lari <mazacon@pm.me>
2024-01-01 20:49:44 -0600

    Debug stuff

--- deploy_maz_dev.sh
+++ deploy_maz_dev.sh
@@ -3,7 +3,14 @@
 echo "Deploying maz.dev"
 
 pkill -f maz.dev
+if [ $? -eq 0 ]; then
+  echo "Killed maz.dev"
+else
+  echo "maz.dev not running"
+fi
 cd ../maz.dev
+echo "Should be in maz.dev"
+pwd
 git stash
 git pull -f
 ruby main.rb > /dev/null 2>&1 &
